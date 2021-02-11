15da99680a883f96555c3cce4fdacaacace95809
Nicolai Gram <nicolaigram@gmail.com>
2021-02-11 11:35:07 +0100

    Working with proxy

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,19 @@
-import * as http from 'http'
-import { enableCors } from './util/cors';
+import * as httpProxy from 'http-proxy'
 const PORT = 5002;
 
-http.createServer((req, res) => {
-    enableCors(res);
-    const driverServiceURL = "http://localhost:5001"
-    const path = req.url;
-    console.log(path)
-    res.writeHead(302, {
-        'Location': driverServiceURL + path
-    })
-    res.end();
-}).listen(PORT)
+const proxy = httpProxy.createProxyServer({target:'http://localhost:5001'})
+proxy.on("proxyRes", (proxyReq, req, res) => {
+    console.log("Request")
+})
+proxy.listen(PORT);
+
+// http.createServer((req, res) => {
+//     enableCors(res);
+//     const driverServiceURL = "http://localhost:5001"
+//     const path = req.url;
+//     proxy.web(req, res), 
+//     // res.writeHead(302, {
+//     //     'Location': driverServiceURL + path
+//     // })
+//     // res.end();
+// }).listen(PORT);
