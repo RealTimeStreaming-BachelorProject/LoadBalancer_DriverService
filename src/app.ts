099b31e7ff50ff43d7d1b13fdb4ed7a52b585e45
Nicolai Gram <nicolaigram@gmail.com>
2021-02-11 14:06:30 +0100

    Clientside loadbalancing

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,23 @@
-import * as httpProxy from 'http-proxy'
+import * as http from 'http'
+import { enableCors } from './util/cors';
 const PORT = 5002;
 
-const proxy = httpProxy.createProxyServer({target:'http://localhost:5001'})
-proxy.on("proxyRes", (proxyReq, req, res) => {
-    console.log("Request")
-})
-proxy.listen(PORT);
+// const proxy = httpProxy.createProxyServer({target:'http://localhost:5001'})
+// proxy.on("proxyRes", (proxyReq, req, res) => {
+//     console.log("Request")
+// })
+// proxy.listen(PORT);
 
-// http.createServer((req, res) => {
-//     enableCors(res);
-//     const driverServiceURL = "http://localhost:5001"
-//     const path = req.url;
-//     proxy.web(req, res), 
-//     // res.writeHead(302, {
-//     //     'Location': driverServiceURL + path
-//     // })
-//     // res.end();
-// }).listen(PORT);
+http.createServer((req, res) => {
+    enableCors(res);
+    const driverServiceURLs = ["http://localhost:5001"]
+    const path = req.url;
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    const newUrl = driverServiceURLs[0] + path;
+    res.write(JSON.stringify({url: newUrl}))
+    // res.writeHead(302, {
+    //     'Location': driverServiceURL + path
+    // })
+    res.end();
+}).listen(PORT);
